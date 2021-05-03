03ef1b1baa282e42d6ec48104882b42903b9235c
Kush Desai <17522@burnside.school.nz>
2021-05-04 11:08:01 +1200

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'hello'
+
+if __name__ == '__main__':
+    app.run(debug=True) 
