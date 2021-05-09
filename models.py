9fe20788b3a71e717a16b645efe1350d28555e02
Kush Desai <17522@burnside.school.nz>
2021-05-10 11:59:27 +1200

    fixed database connection. Models.py now working properly

--- models.py
+++ models.py
@@ -1,12 +1,23 @@
 from main import db
 
+Jobs = db.Table('Jobs', db.Model.metadata, 
+    db.Column('cid',db.Integer, db.ForeignKey('Careers.id')), 
+    db.Column('did',db.Integer, db.ForeignKey('Degrees.id')))
+
+
+Prerequisites = db.Table('Prerequisites', db.Model.metadata, 
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('name', db.String()),
+    db.Column('uid', db.ForeignKey('University.id')),
+    db.Column('did', db.ForeignKey('Degrees.id')))
+
 class Degrees(db.Model):
     __tablename__ = 'Degrees'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
     cost = db.Column(db.Integer())
 
-    universities = db.relationship('degrees', secondary=universities, back_populates='universities')
+    #universities = db.relationship('degrees', secondary=Prerequisites, back_populates='universities')
 
 class University(db.Model):
     __tablename__ = 'University'
@@ -15,7 +26,7 @@ class University(db.Model):
     details = db.Column(db.String())
     location = db.Column(db.String())
 
-    degrees = db.relationship('degrees', secondary=Prerequisites, back_populates='degrees')
+    #degrees = db.relationship('degrees', secondary=Prerequisites, back_populates='degrees')
 
 class Careers(db.Model):
     __tablename__ = 'Careers'
@@ -23,15 +34,6 @@ class Careers(db.Model):
     name = db.Column(db.String())
     pay = db.Column(db.Integer())
 
-    degrees = db.relationship('degrees', secondary=Jobs, back_populates='degrees')
+    #degrees = db.relationship('degrees', secondary=Jobs, back_populates='degrees')
 
-Jobs = db.Table('Jobs', db.Model.metadata, 
-    db.Coloumn('cid',db.Integer, db.ForeignKey('Careers.id')), 
-    db.Coloumn('did',db.Integer, db.ForeignKey('Degrees.id')))
-
-
-Prerequisites = db.Table('Prerequisites', db.Model.metadata, 
-    db.Column('id', db.Integer, primary_key=True),
-    db.Column('name', db.String()),
-    db.Coloumn('uid', db.ForeignKey('University.id')),
-    db.Coloumn('did', db.ForeignKey('Degrees.id')))
+#db.create_all()
